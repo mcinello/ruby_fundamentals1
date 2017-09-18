dcf891f355f86ef1312f09dde84e082e0740fbd2
Michelle Cinello <m.cinello@gmail.com>
2017-09-18 14:25:13 -0400

    used unless expression

--- exercise4.rb
+++ exercise4.rb
@@ -3,10 +3,6 @@ puts "Type any numbers"
 x = gets.to_i
 y = gets.to_i
 
-if x > y
-  puts "x is greater than y!"
-elsif x < y
-  puts "x is less than y!"
-else
-  puts "x equals y!"
+unless x == 10
+  puts "I get printed!"
 end
