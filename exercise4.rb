192cd98dd3a374c329aedaf6edc562b35fb4172e
Michelle Cinello <m.cinello@gmail.com>
2017-09-18 14:22:58 -0400

    used elsif in statement

--- exercise4.rb
+++ exercise4.rb
@@ -1,9 +1,12 @@
-puts "Type any number"
+puts "Type any numbers"
 
-number = gets.to_i
+x = gets.to_i
+y = gets.to_i
 
-if number > 0
-  puts "#{number} is a big number!"
-else number >= 100
-  puts "why dream a little bigger #{number}?"
+if x > y
+  puts "x is greater than y!"
+elsif x < y
+  puts "x is less than y!"
+else
+  puts "x equals y!"
 end
