cad59f9e52fccf1ca927854593931cac4b26216f
Michelle Cinello <m.cinello@gmail.com>
2017-09-18 17:14:52 -0400

    changed the while loop to until loop; used user prompts and a user-distance cap to stop ruby for asking for input

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,6 @@
  user_distance = 0
 
-while user_distance >= 0
+until user_distance >= 20
   puts "Walk or Run?"
   user_action = gets.chomp
 
@@ -10,5 +10,7 @@ while user_distance >= 0
   elsif user_action == "run" || user_action == "Run"
     user_distance += 5
     puts "Distance from home is #{user_distance}km"
+  elsif user_action == "Go home"
+    puts "Time to go home!"
   end
-end 
+end
---
+++ exercise6_1.rb
@@ -0,0 +1,18 @@
+user_distance = 0
+
+until user_distance >= 20
+ puts "Walk or Run?"
+ user_action = gets.chomp
+
+ if user_action == "walk" || user_action == "Walk"
+   user_distance += 1
+   puts "Distance from home is #{user_distance}km"
+ elsif user_action == "run" || user_action == "Run"
+   user_distance += 5
+   puts "Distance from home is #{user_distance}km"
+ elsif user_action == "Go home"
+   puts "Going home!"
+   break
+  #  user_distance -
+ end
+ end
