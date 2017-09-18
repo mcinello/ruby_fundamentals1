39abf3cea26e5c077e0f0445c0911e59effb2883
Michelle Cinello <m.cinello@gmail.com>
2017-09-18 12:59:06 -0400

    Learned about more escape sequences

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,7 @@
-puts "Ada Lovelace lived for #{1852-1815} years."
+puts "Hello\t\tworld"
 
-#Ada Lovelace lived for 37 years.
+puts "Hello\b\b\b\b\bGoodbye world"
+
+puts "Hello\rStart over world"
+
+puts "1. Hello\n2. world"
