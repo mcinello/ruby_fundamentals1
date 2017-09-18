332c7626257ebdca7d67abbc4222d6ae968421bc
Michelle Cinello <m.cinello@gmail.com>
2017-09-18 15:58:26 -0400

    did first while loop; while loop must come first, prompt user, then follow with if statement

---
+++ exercise5.rb
@@ -0,0 +1,14 @@
+ user_distance = 0
+
+while user_distance >= 0
+  puts "Walk or Run?"
+  user_action = gets.chomp
+
+  if user_action == "walk" || user_action == "Walk"
+    user_distance += 1
+    puts "Distance from home is #{user_distance}km"
+  elsif user_action == "run" || user_action == "Run"
+    user_distance += 5
+    puts "Distance from home is #{user_distance}km"
+  end
+end
